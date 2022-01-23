a98083adc981bd363fbb4894d21d01795a1ba90e
Mahatoarabinda <97575974+Mahatoarabinda@users.noreply.github.com>
2022-01-23 10:49:53 +0530

    Add files via upload

---
+++ Switch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter a value:");
+	scanf("%d",&a);
+	switch(10)
+	{
+		case 1 :
+		   printf("Hii");
+		   break;
+	    case 2 :
+			printf("Hello");
+			break;
+	    case 3 :
+			printf("Wolrd");
+			break;
+    	default :
+			printf("Good");	
+	}
+
+}
