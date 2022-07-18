3a45e7eb9711af7fd0e18f9d9b0ea529acf23957
Mahatoarabinda <97575974+Mahatoarabinda@users.noreply.github.com>
2022-07-18 19:55:50 +0530

    Add files via upload

---
+++ Function call.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void message();      /*function prototype declaration*/
+int main()
+{
+	message();      /*function call*/
+	printf("Cry,and you stop to the monotony!\n");
+	return 0;
+	
+}
+void message()       /*fuction defination*/
+{
+	printf("Smile,and the world smiles with you...\n");
+}
